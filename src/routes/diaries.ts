8f1cc69bb4b66f575ccd977fd3628e3a1a08c8b5
Marek Zelinka <mzelinka17@gmail.com>
2024-08-15 11:07:55 +0200

    setup diaries

---
+++ src/routes/diaries.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import diaryService from '../services/diaryService';
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+  const diaryEntries = diaryService.getEntries();
+
+  res.send(diaryEntries);
+});
+
+router.post('/', (_req, res) => {
+  res.send('Saving a diary!');
+});
+
+export default router;
---
+++ src/services/diaryService.ts
@@ -0,0 +1,14 @@
+import diaries from '../../data/entries';
+
+const getEntries = () => {
+  return diaries;
+};
+
+const addDiary = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addDiary,
+};
